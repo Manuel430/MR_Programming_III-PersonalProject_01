b02e78d072b645ff6043c7efe2530258c651d6ba
Manuel Rodriguez <mgrodri5@student.uiwtx.edu>
2023-08-29 17:10:50 -0500

    Might change to RigidBody afterwards

--- Assets/_MyFiles/Misc/PlayerControls.cs
+++ Assets/_MyFiles/Misc/PlayerControls.cs
@@ -44,6 +44,15 @@ public partial class @PlayerControlsScript: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""Value"",
+                    ""id"": ""0a8e1f08-69b2-4ccb-ace2-77efae57f718"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
                 }
             ],
             ""bindings"": [
@@ -112,6 +121,17 @@ public partial class @PlayerControlsScript: IInputActionCollection2, IDisposable
                     ""action"": ""Movement"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""bf7a619a-2afe-4cc6-afa9-778d1e9ccad0"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -122,6 +142,7 @@ public partial class @PlayerControlsScript: IInputActionCollection2, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
         m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -185,12 +206,14 @@ public partial class @PlayerControlsScript: IInputActionCollection2, IDisposable
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Jump;
     private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Look;
     public struct PlayerActions
     {
         private @PlayerControlsScript m_Wrapper;
         public PlayerActions(@PlayerControlsScript wrapper) { m_Wrapper = wrapper; }
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
         public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -206,6 +229,9 @@ public partial class @PlayerControlsScript: IInputActionCollection2, IDisposable
             @Movement.started += instance.OnMovement;
             @Movement.performed += instance.OnMovement;
             @Movement.canceled += instance.OnMovement;
+            @Look.started += instance.OnLook;
+            @Look.performed += instance.OnLook;
+            @Look.canceled += instance.OnLook;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -216,6 +242,9 @@ public partial class @PlayerControlsScript: IInputActionCollection2, IDisposable
             @Movement.started -= instance.OnMovement;
             @Movement.performed -= instance.OnMovement;
             @Movement.canceled -= instance.OnMovement;
+            @Look.started -= instance.OnLook;
+            @Look.performed -= instance.OnLook;
+            @Look.canceled -= instance.OnLook;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -237,5 +266,6 @@ public partial class @PlayerControlsScript: IInputActionCollection2, IDisposable
     {
         void OnJump(InputAction.CallbackContext context);
         void OnMovement(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
     }
 }
--- Assets/_MyFiles/Scripts/MR_PlayerScript.cs
+++ Assets/_MyFiles/Scripts/MR_PlayerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,13 +8,37 @@ public class MR_PlayerScript : MonoBehaviour
 {
     PlayerControlsScript playerControls;
     CharacterController playerController;
-    public float speed = 5f;
+    public float speed = 10f;
+    Vector3 playerVelocity;
+    public bool isGrounded;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3f;
 
-    private void Start()
+    public Camera playerCam;
+    float xRotation = 0f;
+    public float xSensitivity = 30f;
+    public float ySensitivity = 30f;
+
+    private void Awake()
     {
         playerControls = new PlayerControlsScript();
         playerControls.Player.Enable();
         playerController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        isGrounded = playerController.isGrounded;
+
+        if(playerController.isGrounded )
+        {
+            Debug.Log("I am grounded");
+        }
     }
 
     private void FixedUpdate()
@@ -21,11 +46,42 @@ public class MR_PlayerScript : MonoBehaviour
         ProcessMovement();
     }
 
+    private void LateUpdate()
+    {
+        ProcessLook();
+    }
+
     public void ProcessMovement()
     {
         Vector2 inputVector = playerControls.Player.Movement.ReadValue<Vector2>();
         Vector3 movementDirection = new Vector3(inputVector.x, 0, inputVector.y);
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        if (isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2f;
+
         playerController.Move(transform.TransformDirection(movementDirection) * speed * Time.deltaTime);
+    }
+
+    public void ProcessLook()
+    {
+        Vector2 lookVector = playerControls.Player.Look.ReadValue<Vector2>();
+        xRotation -= (lookVector.y * Time.deltaTime) * ySensitivity;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        playerCam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.Rotate(Vector3.up * (lookVector.x * Time.deltaTime) * xSensitivity);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            Debug.Log("Jump: " + context);
+            if(isGrounded)
+            {
+                playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+            }
+        }
     }
 
     public void MovementDebug(InputAction.CallbackContext context)
