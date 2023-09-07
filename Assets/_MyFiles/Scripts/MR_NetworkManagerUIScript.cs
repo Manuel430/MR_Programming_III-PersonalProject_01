81c67056253de444102badb1a209e4a8b1daeac1
Manuel Rodriguez <mgrodri5@student.uiwtx.edu>
2023-09-07 11:29:13 -0500

    Level Layout done, code just need some cleaning.

--- Assets/_MyFiles/Scripts/MR_NetworkManagerUIScript.cs
+++ Assets/_MyFiles/Scripts/MR_NetworkManagerUIScript.cs
@@ -13,9 +13,6 @@ public class MR_NetworkManagerUIScript : MonoBehaviour
     [SerializeField] private Button Area_01Button;
     [SerializeField] private Button Area_02Button;
     [SerializeField] private Button Area_03Button;
-    public GameObject arena_01;
-    public GameObject arena_03;
-    public GameObject arena_02;
 
     private void Awake()
     {
@@ -41,27 +38,6 @@ public class MR_NetworkManagerUIScript : MonoBehaviour
             Area_02Button.gameObject.SetActive(false);
             Area_03Button.gameObject.SetActive(false);
             NetworkManager.Singleton.StartClient();
-        });
-
-        Area_01Button.onClick.AddListener(() =>
-        {
-            arena_01.gameObject.SetActive(true);
-            arena_02.gameObject.SetActive(false);
-            arena_03.gameObject.SetActive(false);
-        });
-
-        Area_02Button.onClick.AddListener(() =>
-        {
-            arena_01.gameObject.SetActive(true);
-            arena_02.gameObject.SetActive(false);
-            arena_03.gameObject.SetActive(false);
-        });
-
-        Area_03Button.onClick.AddListener(() =>
-        {
-            arena_01.gameObject.SetActive(true);
-            arena_02.gameObject.SetActive(false);
-            arena_03.gameObject.SetActive(false);
         });
     }
 }
